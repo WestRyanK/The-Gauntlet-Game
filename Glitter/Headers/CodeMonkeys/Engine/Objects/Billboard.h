2319394b7ab6463fd6c23e6b4f4f49e911d1b2cb
westryank <west.ryan.k@gmail.com>
2018-11-24 02:22:29 -0700

    Add Billboard Effect
    To add a billboard (sprite that always faces camera) to
    the scene, do:
    Billboard* billboard = new Billboard("name",
    	new Texture("path/to/texture.png"),
    	world_width, world_height);
    this->world_root->add_child(billboard);
    
    You can set the billboard's position and velocity,
    but don't change its rotation or you will mess up the effect.

---
+++ Glitter/Headers/CodeMonkeys/Engine/Objects/Billboard.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "glitter.hpp"
+#include "CodeMonkeys/Engine/Objects/PhysicalObject3D.h"
+#include "CodeMonkeys/Engine/Assets/Texture.h"
+#include <string>
+
+using namespace std;
+using namespace CodeMonkeys::Engine::Assets;
+
+namespace CodeMonkeys::Engine::Objects
+{
+    class Billboard : public PhysicalObject3D
+    {
+    private:
+        static ShaderProgram* billboard_shader;
+        static GLuint* billboard_vao;
+        static void init_billboard();
+
+        Texture* billboard_texture;
+        float width;
+        float height;
+    public:
+        Billboard(std::string name, Texture* billboard_texture, float width, float height);
+
+        virtual void draw(mat4 total_transform, ShaderProgram* shader);
+        static ShaderProgram* get_shader();
+    };
+}
