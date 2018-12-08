227ed724533f4933a690bea70c304f412434cadc
westryank <west.ryan.k@gmail.com>
2018-12-08 04:32:59 -0700

    Modify boundary checker
    Boundary checker no longer bounces the ship when it hits the wall,
    it is less disorienting. Also, I standardized the BoundaryChecker
    so it uses vec3s of positive/negative_bounds. It allows us to loop
    over x,y,z instead of dealing with each one on its own.

--- Glitter/Sources/CodeMonkeys/Engine/Collision/BoundaryChecker.cpp
+++ Glitter/Sources/CodeMonkeys/Engine/Collision/BoundaryChecker.cpp
@@ -2,6 +2,12 @@
 
 using namespace CodeMonkeys::Engine::Collision;
 
+BoundaryChecker::BoundaryChecker(vec3 positive_bounds, vec3 negative_bounds)
+{
+    this->positive_bounds = positive_bounds;
+    this->negative_bounds = negative_bounds;
+}
+
 void BoundaryChecker::check_boundary(Object3D* obj)
 {
     if (obj == nullptr) return;
@@ -13,6 +19,7 @@ void BoundaryChecker::check_boundary(Object3D* obj)
 
 void BoundaryChecker::check_boundary(PhysicalObject3D* obj)
 {
+    // std::set<string> valid_objects = {"ship"};
     std::set<string> valid_objects = {"asteroid", "ship"};
     if (valid_objects.count(obj->get_name()) == 0)
         return;
@@ -20,15 +27,33 @@ void BoundaryChecker::check_boundary(PhysicalObject3D* obj)
     vec3 position = obj->get_transformed_position();
     vec3 velocity = obj->get_velocity();
 
-    if (position.x >= max_x && velocity.x > 0 || position.x <= min_x && velocity.x < 0)
-        velocity.x = -velocity.x;
+    for (int i = 0; i < 3; i++)
+    {
+        if (position[i] > positive_bounds[i])
+        {
+            position[i] = positive_bounds[i];
+            if (velocity[i] > 0 && obj->get_name() == "asteroid")
+                velocity[i] = -velocity[i];
 
-    if (position.y >= max_y && velocity.y > 0 || position.y <= min_y && velocity.y < 0)
-        velocity.y = -velocity.y;
+        }
+        if (position[i] < negative_bounds[i])
+        {
+            position[i] = negative_bounds[i];
+            if (velocity[i] < 0 && obj->get_name() == "asteroid")
+                velocity[i] = -velocity[i];
 
-    if (position.z >= max_z && velocity.z > 0 || position.z <= min_z && velocity.z < 0)
-        velocity.z = -velocity.z;
+        }
+    }
+    // if (position.x >= max_x && velocity.x > 0 || position.x <= min_x && velocity.x < 0)
+    //     velocity.x = -velocity.x;
 
+    // if (position.y >= max_y && velocity.y > 0 || position.y <= min_y && velocity.y < 0)
+    //     velocity.y = -velocity.y;
+
+    // if (position.z >= max_z && velocity.z > 0 || position.z <= min_z && velocity.z < 0)
+    //     velocity.z = -velocity.z;
+
+    obj->set_position(position);
     obj->set_velocity(velocity);
 }
 
