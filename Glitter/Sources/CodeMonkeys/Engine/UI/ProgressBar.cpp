874a2bfaff5aa4175f788ceab05473d0c3618204
westryank <west.ryan.k@gmail.com>
2018-12-07 17:37:03 -0700

    Protect ProgressBar from frames out of range

--- Glitter/Sources/CodeMonkeys/Engine/UI/ProgressBar.cpp
+++ Glitter/Sources/CodeMonkeys/Engine/UI/ProgressBar.cpp
@@ -21,6 +21,12 @@ void ProgressBar::update(float current_value, float total, bool reverse)
     {
         frame = (this->texture->get_frame_count() - 1) - frame;
     }
+
+    if (frame > this->texture->get_frame_count())
+        frame = this->texture->get_frame_count();
+    if (frame < 0)
+        frame = 0;
+
     this->texture->set_current_frame(frame);
     this->set_texture(this->texture->get_texture_id());
 }
